b3570e0dee6448cd034c03bc3b57560a002899ce
AcheZeta <ache.mariana@gmail.com>
2019-08-30 18:28:52 -0500

    Function to get the number of day in year

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 const pokeday = document.getElementById('pokeday')
-const pokemonth = document.getElementById('month')
-const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+const pokebithday = document.getElementById("date-pick");
+const clickbtn = document.getElementById("clickkk")
+let userIdPokemon = []
 
-const printMonth = () => {
-    for (i = 0; i < months.length; i++) {
-        pokemonth.insertAdjacentHTML('beforeend', `<div id="pokeMonth">${months[i]}</div>`)
-    }
+const getUserId = () => {
+    let userBirthday = pokebithday.value
+    let parseDate = Date.parse(userBirthday)
+    let startOfYear = userBirthday.slice(0,4)+'-01-01'
+    let parseStart = Date.parse(startOfYear)
+    //You can also use 86400000
+    let days = 1000 * 60 * 60 * 24
+    let diff = parseDate - parseStart 
+    let numberOfDate = Math.ceil(diff/days)
+    userIdPokemon.push(numberOfDate)
+    console.log(userIdPokemon)
 }
-printMonth()
+clickbtn.addEventListener("click", getUserId)
 
-fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=365')
+
+fetch('https://pokeapi.co/api/v2/pokemon/25/')
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        const pokemon = data.results
-        pokemon.forEach(element => {
-            pokeday.insertAdjacentHTML('beforeend', `<div id="pokemon">${element.name}</div>`)
-        });
+        // const pokemon = data.results
+        // console.log(pokemon)
+        // pokemon.forEach(element => {
+        //     pokeday.insertAdjacentHTML('beforeend', `<div class="div1" id="pokemon">${element.name}</div>`)
+        // });
     })
