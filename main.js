f8b6319930ec4b488af0a84f96d36e3243751f9d
AcheZeta <ache.mariana@gmail.com>
2019-08-30 18:48:37 -0500

    Fetch Data from API with the id of user

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
 const pokeday = document.getElementById('pokeday')
 const pokebithday = document.getElementById("date-pick");
 const clickbtn = document.getElementById("clickkk")
-let userIdPokemon = []
+let userIdPokemon = ''
 
 const getUserId = () => {
     let userBirthday = pokebithday.value
     let parseDate = Date.parse(userBirthday)
-    let startOfYear = userBirthday.slice(0,4)+'-01-01'
+    let startOfYear = userBirthday.slice(0, 4) + '-01-01'
     let parseStart = Date.parse(startOfYear)
     //You can also use 86400000
     let days = 1000 * 60 * 60 * 24
-    let diff = parseDate - parseStart 
-    let numberOfDate = Math.ceil(diff/days)
-    userIdPokemon.push(numberOfDate)
+    let diff = parseDate - parseStart
+    let numberOfDate = Math.ceil(diff / days)
+    userIdPokemon = numberOfDate
     console.log(userIdPokemon)
+    getPokemon()
 }
+
 clickbtn.addEventListener("click", getUserId)
 
-
-fetch('https://pokeapi.co/api/v2/pokemon/25/')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        // const pokemon = data.results
-        // console.log(pokemon)
-        // pokemon.forEach(element => {
-        //     pokeday.insertAdjacentHTML('beforeend', `<div class="div1" id="pokemon">${element.name}</div>`)
-        // });
-    })
+const getPokemon = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${userIdPokemon}/`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
